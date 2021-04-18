45d1c3a500dfa65d8beae536992ff1b1fef86ac6
AleksanderData <olekdata@gmail.com>
2021-04-18 23:41:24 +0200

    freeRTOS
    Działa httpd z CGI i SSD
    Działa mqtt
    wszystko pod FreeRTOS

---
+++ Core/Src/freertos.c
@@ -0,0 +1,443 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+// including httpd.h [- HTTPd #1 -]
+#include "lwip/apps/httpd.h"
+
+// we include String.h for the strcmp() function [= CGI #1 =]
+#include <String.h>
+// we include this library to be able to use boolean variables for SSI
+#include <Stdbool.h>
+
+bool LD1ON = false; // this variable will indicate if the LD3 LED on the board is ON or not
+bool LD2ON = false; // this variable will indicate if our LD2 LED on the board is ON or not
+
+#include "mqtt.h"
+
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+// just declaring the function for the compiler [= CGI #2 =]
+const char* LedCGIhandler(int iIndex, int iNumParams, char *pcParam[],
+char *pcValue[]);
+
+// in our SHTML file <form method="get" action="/leds.cgi"> [= CGI #3 =]
+const tCGI LedCGI = { "/leds.cgi", LedCGIhandler };
+
+// [= CGI #4 =]
+tCGI theCGItable[1];
+
+// just declaring SSI handler function [* SSI #1 *]
+u16_t mySSIHandler(int iIndex, char *pcInsert, int iInsertLen);
+
+// [* SSI #2 *]
+#define numSSItags 2
+
+// [* SSI #3 *]
+char const *theSSItags[numSSItags] = { "tag1", "tag2" };
+
+// the actual function for handling CGI [= CGI #5 =]
+const char* LedCGIhandler(int iIndex, int iNumParams, char *pcParam[],
+char *pcValue[]) {
+
+	uint32_t i = 0;
+
+	if (iIndex == 0) {
+
+		//turning the LED lights off
+		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+		// we put this variable to false to indicate that the LD2 LED on the board is not ON
+		LD2ON = false;
+
+		HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+
+		// we put this variable to false to indicate that the LD* LED on the board is not ON
+		LD1ON = false;
+
+	}
+
+	for (i = 0; i < iNumParams; i++) {
+
+		if (strcmp(pcParam[i], "led") == 0)
+
+		{
+
+			if (strcmp(pcValue[i], "1") == 0) {
+
+				HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+
+				// LD3 LED (red) on the board is ON!
+				LD1ON = true;
+
+			}
+
+			else if (strcmp(pcValue[i], "2") == 0) {
+
+				HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+
+				// LD2 LED (blue) on the board is ON!
+				LD2ON = true;
+
+			}
+
+		}
+
+	}
+
+	// the extension .shtml for SSI to work
+	return "/index.shtml";
+
+} // END [= CGI #5 =]
+
+// function to initialize CGI [= CGI #6 =]
+void myCGIinit(void) {
+	//add LED control CGI to the table
+	theCGItable[0] = LedCGI;
+	//give the table to the HTTP server
+	http_set_cgi_handlers(theCGItable, 1);
+} // END [= CGI #6 =]
+
+// the actual function for SSI [* SSI #4 *]
+u16_t mySSIHandler(int iIndex, char *pcInsert, int iInsertLen) {
+
+	if (iIndex == 0) {
+
+		if (LD1ON == false) {
+
+			char myStr1[] = "<input value=\"1\" name=\"led\" type=\"checkbox\">";
+
+			strcpy(pcInsert, myStr1);
+
+			return strlen(myStr1);
+		}
+
+		else if (LD1ON == true) {
+
+			// since the LD3 red LED on the board is ON we make its checkbox checked!
+			char myStr1[] =
+			"<input value=\"1\" name=\"led\" type=\"checkbox\" checked>";
+			strcpy(pcInsert, myStr1);
+
+			return strlen(myStr1);
+
+		}
+
+	}
+
+	else if (iIndex == 1)
+
+	{
+		if (LD2ON == false) {
+			char myStr2[] = "<input value=\"2\" name=\"led\" type=\"checkbox\">";
+			strcpy(pcInsert, myStr2);
+
+			return strlen(myStr2);
+		}
+
+		else if (LD2ON == true) {
+
+			// since the LD2 blue LED on the board is ON we make its checkbox checked!
+			char myStr2[] =
+			"<input value=\"2\" name=\"led\" type=\"checkbox\" checked>";
+			strcpy(pcInsert, myStr2);
+
+			return strlen(myStr2);
+
+		}
+
+	}
+
+	return 0;
+
+}
+
+// function to initialize SSI [* SSI #5 *]
+void mySSIinit(void) {
+
+	http_set_ssi_handler(mySSIHandler, (char const**) theSSItags,
+	numSSItags);
+}
+
+mqtt_client_t mqtt_client;
+
+static void mqtt_sub_request_cb(void *arg, err_t result)
+{
+  /* Just print the result code here for simplicity,
+     normal behaviour would be to take some action if subscribe fails like
+     notifying user, retry subscribe or disconnect from server */
+//  printf("Subscribe result: %d\n", result);
+}
+
+static int inpub_id;
+static void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len)
+{
+//  printf("Incoming publish at topic %s with total length %u\n", topic, (unsigned int)tot_len);
+
+  /* Decode topic string into a user defined reference */
+  if(strcmp(topic, "print_payload") == 0) {
+    inpub_id = 0;
+  } else if(topic[0] == 'A') {
+    /* All topics starting with 'A' might be handled at the same way */
+    inpub_id = 1;
+  } else {
+    /* For all other topics */
+    inpub_id = 2;
+  }
+}
+
+static void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t flags)
+{
+//  printf("Incoming publish payload with length %d, flags %u\n", len, (unsigned int)flags);
+
+  if(flags & MQTT_DATA_FLAG_LAST) {
+    /* Last fragment of payload received (or whole part if payload fits receive buffer
+       See MQTT_VAR_HEADER_BUFFER_LEN)  */
+
+    /* Call function or do action depending on reference, in this case inpub_id */
+    if(inpub_id == 0) {
+      /* Don't trust the publisher, check zero termination */
+      if(data[len-1] == 0) {
+//        printf("mqtt_incoming_data_cb: %s\n", (const char *)data);
+      }
+    } else if(inpub_id == 1) {
+      /* Call an 'A' function... */
+    } else {
+//      printf("mqtt_incoming_data_cb: Ignoring payload...\n");
+    }
+  } else {
+    /* Handle fragmented payload, store in buffer, write to file or whatever */
+  }
+}
+
+
+
+static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status)
+{
+  err_t err;
+  if(status == MQTT_CONNECT_ACCEPTED) {
+//    printf("mqtt_connection_cb: Successfully connected\n");
+
+    /* Setup callback for incoming publish requests */
+    mqtt_set_inpub_callback(client, mqtt_incoming_publish_cb, mqtt_incoming_data_cb, arg);
+
+    /* Subscribe to a topic named "subtopic" with QoS level 1, call mqtt_sub_request_cb with result */
+    err = mqtt_subscribe(client, "subtopic", 1, mqtt_sub_request_cb, arg);
+
+    if(err != ERR_OK) {
+//      printf("mqtt_subscribe return: %d\n", err);
+    }
+  } else {
+//    printf("mqtt_connection_cb: Disconnected, reason: %d\n", status);
+
+    /* Its more nice to be connected, so try to reconnect */
+    example_do_connect(client);
+  }
+}
+
+
+
+void example_do_connect(mqtt_client_t *client, const char *topic)
+{
+  struct mqtt_connect_client_info_t ci;
+  err_t err;
+
+  /* Setup an empty client info structure */
+  memset(&ci, 0, sizeof(ci));
+
+  /* Minimal amount of information required is client identifier, so set it here */
+  ci.client_id = "stm32";
+  //ci.client_user = "mosquitto";
+  //ci.client_pass = "chupasangre"; /* Tiempo en mi caso */
+
+
+  /* Initiate client and connect to server, if this fails immediately an error code is returned
+     otherwise mqtt_connection_cb will be called with connection result after attempting
+     to establish a connection with the server.
+     For now MQTT version 3.1.1 is always used */
+  ip_addr_t mqttServerIP;
+  IP4_ADDR(&mqttServerIP, 192, 168, 1, 200);
+//  err = mqtt_client_connect(client, &mqttServerIP, MQTT_PORT, mqtt_connection_cb, 0, &ci);
+  err = mqtt_client_connect(client, &mqttServerIP, MQTT_PORT, mqtt_connection_cb, &topic, &ci);
+
+  /* For now just print the result code if something goes wrong */
+  if(err != ERR_OK) {
+//    LCD_UsrLog("mqtt_connect return %d\n", err);
+  }
+	else
+	{
+//		LCD_UsrLog("Connected");
+	}
+}
+
+
+/* Called when publish is complete either with sucess or failure */
+static void mqtt_pub_request_cb(void *arg, err_t result)
+{
+  if(result != ERR_OK) {
+//    printf("Publish result: %d\n", result);
+  }
+}
+
+
+void example_publish(mqtt_client_t *client, void *arg)
+{
+  const char *pub_payload= "Test z NUCLEO-F207";
+  err_t err;
+  u8_t qos = 2; /* 0 1 or 2, see MQTT specification */
+  u8_t retain = 0; /* No don't retain such crappy payload... */
+  err = mqtt_publish(client, "DataSoft/stm32", pub_payload, strlen(pub_payload), qos, retain, mqtt_pub_request_cb, arg);
+  if(err != ERR_OK) {
+//    printf("Publish err: %d\n", err);
+  }
+}
+
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+osThreadId defaultTaskHandle;
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void const * argument);
+
+extern void MX_LWIP_Init(void);
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}
+/* USER CODE END GET_IDLE_TASK_MEMORY */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of defaultTask */
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 512);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const * argument)
+{
+  /* init code for LWIP */
+  MX_LWIP_Init();
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+
+  // initializing the HTTPd [-HTTPd #2-]
+  httpd_init();
+
+  // initializing CGI  [= CGI #7 =]
+  myCGIinit();
+
+  // initializing SSI [* SSI #6 *]
+  mySSIinit();
+  example_do_connect(&mqtt_client, 'DataSoft/stm32');
+
+  for(;;)
+  {
+	  HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+	  example_publish(&mqtt_client, 0);
+	  osDelay(1000);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,21 +19,15 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
+#include "can.h"
 #include "lwip.h"
+#include "usart.h"
+#include "usb_otg.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
-// including httpd.h [- HTTPd #1 -]
-#include "lwip/apps/httpd.h"
-
-// we include String.h for the strcmp() function [= CGI #1 =]
-#include <String.h>
-// we include this library to be able to use boolean variables for SSI
-#include <Stdbool.h>
-
-bool LD1ON = false; // this variable will indicate if the LD3 LED on the board is ON or not
-bool LD2ON = false; // this variable will indicate if our LD2 LED on the board is ON or not
 
 /* USER CODE END Includes */
 
@@ -52,173 +46,20 @@ bool LD2ON = false; // this variable will indicate if our LD2 LED on the board i
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-CAN_HandleTypeDef hcan1;
-
-UART_HandleTypeDef huart3;
-
-PCD_HandleTypeDef hpcd_USB_OTG_FS;
 
 /* USER CODE BEGIN PV */
-
-// just declaring the function for the compiler [= CGI #2 =]
-const char* LedCGIhandler(int iIndex, int iNumParams, char *pcParam[],
-char *pcValue[]);
-
-// in our SHTML file <form method="get" action="/leds.cgi"> [= CGI #3 =]
-const tCGI LedCGI = { "/leds.cgi", LedCGIhandler };
-
-// [= CGI #4 =]
-tCGI theCGItable[1];
-
-// just declaring SSI handler function [* SSI #1 *]
-u16_t mySSIHandler(int iIndex, char *pcInsert, int iInsertLen);
-
-// [* SSI #2 *]
-#define numSSItags 2
-
-// [* SSI #3 *]
-char const *theSSItags[numSSItags] = { "tag1", "tag2" };
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_USART3_UART_Init(void);
-static void MX_USB_OTG_FS_PCD_Init(void);
-static void MX_CAN1_Init(void);
+void MX_FREERTOS_Init(void);
 /* USER CODE BEGIN PFP */
-
-int czas = 0;
-
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
-// the actual function for handling CGI [= CGI #5 =]
-const char* LedCGIhandler(int iIndex, int iNumParams, char *pcParam[],
-char *pcValue[]) {
-
-	uint32_t i = 0;
-
-	if (iIndex == 0) {
-
-		//turning the LED lights off
-		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-
-		// we put this variable to false to indicate that the LD2 LED on the board is not ON
-		LD2ON = false;
-
-		HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
-
-		// we put this variable to false to indicate that the LD* LED on the board is not ON
-		LD1ON = false;
-
-	}
-
-	for (i = 0; i < iNumParams; i++) {
-
-		if (strcmp(pcParam[i], "led") == 0)
-
-		{
-
-			if (strcmp(pcValue[i], "1") == 0) {
-
-				HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
-
-				// LD3 LED (red) on the board is ON!
-				LD1ON = true;
-
-			}
-
-			else if (strcmp(pcValue[i], "2") == 0) {
-
-				HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
-
-				// LD2 LED (blue) on the board is ON!
-				LD2ON = true;
-
-			}
-
-		}
-
-	}
-
-	// the extension .shtml for SSI to work
-	return "/index.shtml";
-
-} // END [= CGI #5 =]
-
-// function to initialize CGI [= CGI #6 =]
-void myCGIinit(void) {
-	//add LED control CGI to the table
-	theCGItable[0] = LedCGI;
-	//give the table to the HTTP server
-	http_set_cgi_handlers(theCGItable, 1);
-} // END [= CGI #6 =]
-
-// the actual function for SSI [* SSI #4 *]
-u16_t mySSIHandler(int iIndex, char *pcInsert, int iInsertLen) {
-
-	if (iIndex == 0) {
-
-		if (LD1ON == false) {
-
-			char myStr1[] = "<input value=\"1\" name=\"led\" type=\"checkbox\">";
-
-			strcpy(pcInsert, myStr1);
-
-			return strlen(myStr1);
-		}
-
-		else if (LD1ON == true) {
-
-			// since the LD3 red LED on the board is ON we make its checkbox checked!
-			char myStr1[] =
-			"<input value=\"1\" name=\"led\" type=\"checkbox\" checked>";
-			strcpy(pcInsert, myStr1);
-
-			return strlen(myStr1);
-
-		}
-
-	}
-
-	else if (iIndex == 1)
-
-	{
-		if (LD2ON == false) {
-			char myStr2[] = "<input value=\"2\" name=\"led\" type=\"checkbox\">";
-			strcpy(pcInsert, myStr2);
-
-			return strlen(myStr2);
-		}
-
-		else if (LD2ON == true) {
-
-			// since the LD2 blue LED on the board is ON we make its checkbox checked!
-			char myStr2[] =
-			"<input value=\"2\" name=\"led\" type=\"checkbox\" checked>";
-			strcpy(pcInsert, myStr2);
-
-			return strlen(myStr2);
-
-		}
-
-	}
-
-	return 0;
-
-}
-
-// function to initialize SSI [* SSI #5 *]
-void mySSIinit(void) {
-
-	http_set_ssi_handler(mySSIHandler, (char const**) theSSItags,
-	numSSItags);
-}
 
 /* USER CODE END 0 */
 
@@ -253,33 +94,20 @@ int main(void)
   MX_USART3_UART_Init();
   MX_USB_OTG_FS_PCD_Init();
   MX_CAN1_Init();
-  MX_LWIP_Init();
   /* USER CODE BEGIN 2 */
-
-  // initializing the HTTPd [-HTTPd #2-]
- httpd_init();
-
- // initializing CGI  [= CGI #7 =]
- myCGIinit();
-
- // initializing SSI [* SSI #6 *]
- mySSIinit();
 
   /* USER CODE END 2 */
 
+  /* Call init function for freertos objects (in freertos.c) */
+  MX_FREERTOS_Init();
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  MX_LWIP_Process();
-
-	  if ((HAL_GetTick() - czas) > 1000) {
-		  HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
-		  czas = HAL_GetTick();
-	  }
-
-
-
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -327,171 +155,30 @@ void SystemClock_Config(void)
   }
 }
 
-/**
-  * @brief CAN1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_CAN1_Init(void)
-{
-
-  /* USER CODE BEGIN CAN1_Init 0 */
-
-  /* USER CODE END CAN1_Init 0 */
-
-  /* USER CODE BEGIN CAN1_Init 1 */
-
-  /* USER CODE END CAN1_Init 1 */
-  hcan1.Instance = CAN1;
-  hcan1.Init.Prescaler = 16;
-  hcan1.Init.Mode = CAN_MODE_NORMAL;
-  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_1TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_1TQ;
-  hcan1.Init.TimeTriggeredMode = DISABLE;
-  hcan1.Init.AutoBusOff = DISABLE;
-  hcan1.Init.AutoWakeUp = DISABLE;
-  hcan1.Init.AutoRetransmission = DISABLE;
-  hcan1.Init.ReceiveFifoLocked = DISABLE;
-  hcan1.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN CAN1_Init 2 */
-
-  /* USER CODE END CAN1_Init 2 */
-
-}
-
-/**
-  * @brief USART3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART3_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART3_Init 0 */
-
-  /* USER CODE END USART3_Init 0 */
-
-  /* USER CODE BEGIN USART3_Init 1 */
-
-  /* USER CODE END USART3_Init 1 */
-  huart3.Instance = USART3;
-  huart3.Init.BaudRate = 115200;
-  huart3.Init.WordLength = UART_WORDLENGTH_8B;
-  huart3.Init.StopBits = UART_STOPBITS_1;
-  huart3.Init.Parity = UART_PARITY_NONE;
-  huart3.Init.Mode = UART_MODE_TX_RX;
-  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART3_Init 2 */
-
-  /* USER CODE END USART3_Init 2 */
-
-}
-
-/**
-  * @brief USB_OTG_FS Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USB_OTG_FS_PCD_Init(void)
-{
-
-  /* USER CODE BEGIN USB_OTG_FS_Init 0 */
-
-  /* USER CODE END USB_OTG_FS_Init 0 */
-
-  /* USER CODE BEGIN USB_OTG_FS_Init 1 */
-
-  /* USER CODE END USB_OTG_FS_Init 1 */
-  hpcd_USB_OTG_FS.Instance = USB_OTG_FS;
-  hpcd_USB_OTG_FS.Init.dev_endpoints = 4;
-  hpcd_USB_OTG_FS.Init.speed = PCD_SPEED_FULL;
-  hpcd_USB_OTG_FS.Init.dma_enable = DISABLE;
-  hpcd_USB_OTG_FS.Init.phy_itface = PCD_PHY_EMBEDDED;
-  hpcd_USB_OTG_FS.Init.Sof_enable = ENABLE;
-  hpcd_USB_OTG_FS.Init.low_power_enable = DISABLE;
-  hpcd_USB_OTG_FS.Init.vbus_sensing_enable = ENABLE;
-  hpcd_USB_OTG_FS.Init.use_dedicated_ep1 = DISABLE;
-  if (HAL_PCD_Init(&hpcd_USB_OTG_FS) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USB_OTG_FS_Init 2 */
-
-  /* USER CODE END USB_OTG_FS_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOG_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : USER_Btn_Pin */
-  GPIO_InitStruct.Pin = USER_Btn_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LD1_Pin */
-  GPIO_InitStruct.Pin = LD1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : LD3_Pin LD2_Pin */
-  GPIO_InitStruct.Pin = LD3_Pin|LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : USB_PowerSwitchOn_Pin */
-  GPIO_InitStruct.Pin = USB_PowerSwitchOn_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(USB_PowerSwitchOn_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : USB_OverCurrent_Pin */
-  GPIO_InitStruct.Pin = USB_OverCurrent_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(USB_OverCurrent_GPIO_Port, &GPIO_InitStruct);
-
-}
-
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
+
+ /**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
